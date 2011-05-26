6102c521f71ef8f0d9768c89b1a2e1452e9e9246
Linuxmotion <linuxmotion@thelinuxmotion.org>
2011-05-25 19:28:32 -0700

    Add layout stuff for th music controsl. Save before going crazy on testing

--- gen/com/android/internal/widget/R.java
+++ gen/com/android/internal/widget/R.java
@@ -60,7 +60,8 @@ public final class R {
         public static final int landscape=0x7f030001;
         public static final int main=0x7f030002;
         public static final int music_controls=0x7f030003;
-        public static final int play_pause_selector=0x7f030004;
+        public static final int musiccontrols=0x7f030004;
+        public static final int play_pause_selector=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
--- src/com/android/internal/widget/MusicControlsLayout.java
+++ src/com/android/internal/widget/MusicControlsLayout.java
@@ -59,6 +59,144 @@ public class MusicControlsLayout extends RelativeLayout implements OnClickListen
 
         
     }
+    
+   
+    @Override
+    public void onFinishInflate(){
+    	super.onFinishInflate();
+    	
+    	
+    	 
+    	 mLayout = (RelativeLayout) this.findViewById(R.id.music_controls_layout);
+    	 /*
+    	 mPlayPauseButton = (ImageView) mLayout.findViewById(R.id.play_pause_button);
+    	 mFastFowardButton = (ImageView) mLayout.findViewById(R.id.fast_forward_button);
+    	 mRewindButton = (ImageView) mLayout.findViewById(R.id.rewind_button);
+    	 
+    	 
+    	
+        this.mPlayPauseButton.setOnClickListener(this);
+        this.mFastFowardButton.setOnClickListener(this);
+        this.mRewindButton.setOnClickListener(this);
+    	 
+   
+    	   
+    	 mRewindButton.setImageBitmap(getBitmapFor(R.drawable.lock_ic_media_previous));
+    	 mPlayPauseButton.setImageBitmap(getBitmapFor(R.drawable.lock_ic_media_play));
+    	 mFastFowardButton.setImageBitmap(getBitmapFor(R.drawable.lock_ic_media_next));
+    	 */
+    }
+    
+    @Override
+    protected void onAttachedToWindow() {
+    	super.onAttachedToWindow();
+    	
+    	  
+   	 
+    	// Register call backs
+    	  if (IDBG) log("Attching " + TAG + " to the window");
+		  
+		  	 mContext = this.getContext();
+			
+			 IntentFilter iF = new IntentFilter();
+			 iF.addAction("com.android.music.metachanged");
+			 iF.addAction("com.android.music.playstatechanged");
+			 
+			 // Register if the music play state has change
+			 mContext.registerReceiver(mMusicReceiver, iF);
+    	
+
+    }
+    
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        
+        // unregister call backs
+
+		  mContext = this.getContext();
+		  mContext.unregisterReceiver(mMusicReceiver);
+		  
+		  mContext = null;
+        
+    }
+    
+    public void onClick(View v) {
+		// TODO Auto-generated method stub
+		log("A view has been clicked");
+		
+		
+		if(v.equals(mRewindButton)){
+			
+			
+		}
+
+		if(v.equals(this.mPlayPauseButton)){
+			
+			
+		}
+
+		if(v.equals(this.mFastFowardButton)){
+			
+			
+		}
+
+		
+		
+	}
+    
+    
+
+	private BroadcastReceiver mMusicReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			         String action = intent.getAction();
+	            mArtist = intent.getStringExtra("artist");
+	            mTrack = intent.getStringExtra("track");
+	            mIsMusicPlaying = intent.getBooleanExtra("playing", false);
+	            mSongId = intent.getLongExtra("songid", 0);
+	            mAlbumId = intent.getLongExtra("albumid", 0);
+	            
+	            // Update the lock screen music controls here
+	            intent = new Intent("internal.policy.impl.updateSongStatus");
+	            
+	            // Send the broadcast signaling that the lockscreen should update the controls
+	            context.sendBroadcast(intent);
+		}
+
+    };
+    
+    
+    
+	   public void sendMediaButtonEvent(int code) {
+		   
+
+			  if (IDBG) log("sending media button event");
+	        long eventtime = SystemClock.uptimeMillis();
+
+	        Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+	        KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, code, 0);
+	        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+	        getContext().sendOrderedBroadcast(downIntent, null);
+
+	        Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+	        KeyEvent upEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_UP, code, 0);
+	        upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+	        getContext().sendOrderedBroadcast(upIntent, null);
+	    }
+    
+    private Bitmap getBitmapFor(int resId) {
+        return BitmapFactory.decodeResource(getContext().getResources(), resId);
+    }
+    
+    
+    
+ // Debugging / testing code
+
+	private void log(String msg) {
+	    Log.d(TAG, msg);
+	}
 	    
 
 }
